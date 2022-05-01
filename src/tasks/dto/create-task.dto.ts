3b37d58d0379e33c43869ec19dcb71474b38d659
Jose Guilherme <jgnvs6@gmail.com>
2022-04-30 22:27:52 -0300

    feat: validate days if type is daily

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,5 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsBoolean, IsEnum, IsIn, IsInt, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import {
+  IsEnum,
+  IsIn,
+  IsInt,
+  IsNotEmpty,
+  IsString,
+  ValidateIf,
+} from 'class-validator';
 import {
   Day,
   dayArray,
@@ -23,17 +30,23 @@ export class CreateTaskDto {
   @IsNotEmpty()
   type: TaskType;
 
-  @ApiProperty({ enum: dayArray, isArray: true, example: ['sunday', 'monday'], required: false })
+  @ValidateIf((task) => task.type === 'daily')
+  @ApiProperty({
+    enum: dayArray,
+    isArray: true,
+    example: ['sunday', 'monday'],
+    required: false,
+  })
   @IsEnum(dayArray, { each: true })
-  @IsOptional()
+  @IsNotEmpty()
   days?: Day[];
 
-  @ApiProperty({example: "87114373-b7de-4e48-b603-df97545a7482"})
+  @ApiProperty({ example: '87114373-b7de-4e48-b603-df97545a7482' })
   @IsString()
   @IsNotEmpty()
   createdById: string;
 
-  @ApiProperty({ example: "a9795b14-af93-4db0-89f2-9742ac9b6d13"})
+  @ApiProperty({ example: 'a9795b14-af93-4db0-89f2-9742ac9b6d13' })
   @IsString()
   @IsNotEmpty()
   createdForId: string;
